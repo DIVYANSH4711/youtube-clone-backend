bf678d3140cdc742acf37a1c71057ca34914e62b
Divyansh Kashyap <duvyansh728@gmail.com>
2025-02-24 18:51:23 +0530

    subscription controller done

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/:username/dashboard", dashboardRouter)
+app.use("/api/v1/u/:username/dashboard", dashboardRouter)
 
 // http://localhost:8000/api/v1/users/register
 
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,17 +9,195 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const { channelId } = req.params
     // TODO: toggle subscription
+    if (!isValidObjectId(channelId))
+        throw new ApiError(
+            400,
+            "Invalid channel id. Please provide a valid channel id"
+        )
     
+    const channel = await User.findOne({ _id: channelId })
+
+    if (!channel)
+        throw new ApiError(
+            404,
+            "Channel not found. Please provide a valid channel id"
+        )
+    
+    const ifAlreadySubscribed = await Subscription.findOne({
+        channel: channelId,
+        subscriber: req.user._id
+    })
+
+    if (ifAlreadySubscribed) {
+        await Subscription.findByIdAndDelete(ifAlreadySubscribed._id)
+        return res
+            .status(200)
+            .json(new ApiResponse(
+                200,
+                {},
+                "Unsubscribed successfully"
+            ))
+    }
+
+    const subscription = await Subscription.create({
+        channel: channelId,
+        subscriber: req.user._id
+    })
+
+    if (!subscription)
+        throw new ApiError(
+                500, 
+                "An error occurred while subscribing to the channel"
+            )
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                subscription,
+                "Subscribed successfully"
+            )
+        ) 
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const { channelId } = req.params
+    let { page = 1, limit = 10} = req.query
+    page = Number(page)
+    limit = Number(limit)
+    if (!isValidObjectId(channelId))
+        throw new ApiError(
+            400,
+            "Invalid channel id. Please provide a valid channel id"
+        )
+
+    const channel = await User.findOne({
+        _id: channelId
+    })
+
+    if (!channel)
+        throw new ApiError(
+            404,
+            "Channel not found. Please provide a valid channel id"
+        )
+    
+    const subscribers = await Subscription.aggregatePaginate([
+        {
+            $match: {
+                channel: mongoose.Types.ObjectId(channel._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscriber",   
+                foreignField: "_id",
+                as: "subscriber"
+            }
+        },
+        {
+            $unwind: "$subscriber"
+        },
+        {
+            $project: {
+                "subscriber.password": 0,
+                "subscriber.email": 0,
+                "subscriber.createdAt": 0,
+                "subscriber.updatedAt": 0,
+                "subscriber.refreshToken": 0,
+                "subscriber.watchHistory": 0,
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $skip: (page - 1) * limit
+        },
+        {
+            $limit: limit
+        }
+    ])
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    let { page = 1, limit = 10 } = req.query
+    page = Number(page)
+    limit = Number(limit)
+    if (!isValidObjectId(subscriberId))
+        throw new ApiError(
+            400,
+            "Invalid subscriber id. Please provide a valid subscriber id"
+        )
+    
+    const user = await User.findById(subscriberId)
+
+    if (!user)
+        throw new ApiError(
+            404,
+            "User not found. Please provide a valid user id"
+        )
+    
+    const subscriptions = await Subscription.aggregatePaginate([
+        {
+            $match: {
+                subscriber: mongoose.Types.ObjectId(user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channel"
+            }
+        },
+        {
+            $unwind: "$channel"
+        },
+        {
+            $project: {
+                "channel.password": 0,
+                "channel.email": 0,
+                "channel.createdAt": 0,
+                "channel.updatedAt": 0,
+                "channel.refreshToken": 0,
+                "channel.watchHistory": 0,
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            },
+        },
+        {
+            $skip: (page - 1) * limit
+        },
+        {
+            $limit: limit
+        }
+    ])
+    if (!subscriptions)
+        throw new ApiError(
+            404,
+            "No subscribed channels found for this user"
+        )
+    
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                subscriptions,
+                "Subscribed channels fetched successfully"
+            )
+        )
 })
 
 export {
