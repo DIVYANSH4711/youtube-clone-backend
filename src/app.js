995fe6d05e321260ec386b6d1f4e778bfbd29eb2
Divyansh Kashyap <duvyansh728@gmail.com>
2025-03-01 10:13:12 +0530

    fix small bug-2

--- src/app.js
+++ src/app.js
@@ -6,9 +6,10 @@ import { errorHandler } from './utils/errorHandler.js'
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    origin: "http://localhost:5173", 
+    credentials: true, 
+    allowedHeaders: ["Authorization", "Content-Type"],
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
