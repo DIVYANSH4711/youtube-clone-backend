257a10a27f2767165653db1709248198d327eb97
Divyansh Kashyap <duvyansh728@gmail.com>
2025-03-14 21:45:43 +0530

    minor change

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { errorHandler } from './utils/errorHandler.js'
 const app = express()
 
 app.use(cors({
-    origin: process.env.ORIGIN, 
+    origin: ["https://video-stream-jet.vercel.app/"], 
     credentials: true, 
     allowedHeaders: ["Authorization", "Content-Type"],
 }));
